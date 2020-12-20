43ef011554665b66cfd7354093c181c37c232564
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2020-12-21 00:41:22 +0300

    Create 'InputBorderLabel' component

---
+++ service/src/components/library/input/InputBorderLabel.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- TODO: Make up form -->
+  <!--  <div class="form-group input-group">-->
+  <div class="form-group input-group">
+    <span class="input-label-border">
+      <input :type="[[ type ]]" class="form-control" :id="[[ id ]]" :placeholder="[[ placeholder ]]" required autofocus>
+      <label :for="[[ id ]]">{{ label }}</label>
+    </span>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+
+  // TODO: Add tests for component
+  @Options({
+    props: {
+      id: String,
+      type: String,
+      label: String,
+      placeholder: String
+    }
+  })
+  export default class InputBorderLabel extends Vue {}
+</script>
+
+
+<style lang="scss">
+  .input-label-border {
+    display: block;
+    position: relative;
+  }
+
+  .input-label-border label,
+  .input-label-border > span {
+    position: absolute;
+    cursor: text;
+    font-size: 75%;
+    opacity: 1;
+    -webkit-transition: all .2s;
+            transition: all .2s;
+    top: -.5em;
+    left: 0.75rem;
+    z-index: 3;
+    line-height: 1;
+    padding: 0 1px;
+    // Disable selection for label
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+       -moz-user-select: none;
+        -ms-user-select: none;
+            user-select: none;
+  }
+
+  .input-label-border label::after,
+  .input-label-border > span::after {
+    content: " ";
+    display: block;
+    position: absolute;
+    background: white;
+    height: 2px;
+    top: 50%;
+    left: -.2em;
+    right: -.2em;
+    z-index: -1;
+  }
+</style>
--- service/src/views/auth/SignIn.vue
+++ service/src/views/auth/SignIn.vue
@@ -1,5 +1,30 @@
 <template>
-  <h1>
-    This is sign in page
-  </h1>
+  <form>
+    <input-border-label
+      id="sign-in-email"
+      type="email"
+      label="Email"
+      placeholder="email@example.com"
+    />
+    <input-border-label
+      id="sign-in-password"
+      type="password"
+      label="Password"
+      placeholder="password"
+    />
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 </template>
+
+
+<script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  import InputBorderLabel from "@/components/library/input/InputBorderLabel.vue";
+
+  @Options({
+    components: {
+      InputBorderLabel
+    }
+  })
+  export default class SignIn extends Vue {}
+</script>
