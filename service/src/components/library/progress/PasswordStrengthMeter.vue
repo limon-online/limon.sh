c02a934581a8b628c15d9b78305716f27308772d
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2020-12-22 17:48:36 +0300

    Upgrade component 'PasswordStrongMeter'

--- service/src/components/library/progress/PasswordStrengthMeter.vue
+++ service/src/components/library/progress/PasswordStrengthMeter.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="progress">
     <div
-      role="progressbar"
       class="progress-bar"
       v-bind:class="color"
-      v-bind:style="{ width: progress + '%', color: color }"
-      v-bind:aria-valuenow="progress"
-      aria-valuemin="0"
-      aria-valuemax="100"
+      v-bind:style="{ width: low + '%'}"
+    ></div>
+    <div
+      class="progress-bar"
+      v-bind:class="color"
+      v-bind:style="{ width: medium + '%'}"
+    ></div>
+    <div
+      class="progress-bar"
+      v-bind:class="color"
+      v-bind:style="{ width: height + '%'}"
     ></div>
   </div>
 </template>
@@ -25,6 +31,9 @@
 
     data() {
       return {
+        low: 0,
+        medium: 0,
+        height: 0,
         progress: 0,
         color: 'bg-danger'
       }
@@ -34,17 +43,50 @@
       password: function(newVal) {
         this.progress = Math.round((newVal.length / 24) * 100)
 
-        if (this.progress <= 33) {
-          this.color = 'bg-danger'
+        if (this.progress === 0) {
+          this.low = 0
+          this.medium = 0
+          this.height = 0
         }
-        else if (this.progress <=66) {
+
+        // TODO: Fix last section in progress bar
+        console.log(this.progress, this.low, this.medium, this.height)
+
+        if (this.progress >= 0 && this.progress <= 33) {
+          this.color = 'bg-danger'
+          this.low = this.progress
+          this.medium = 0
+          this.height = 0
+        }
+        else if (this.progress >= 34 && this.progress <=66) {
           this.color = 'bg-warning'
+          this.low = 33
+          this.medium = this.progress - 33
+          this.height = 0
         }
         else {
           this.color = 'bg-success'
+          this.low = 33
+          this.medium = 33
+
+          if (this.progress > 100) {
+            this.height = 32
+          }
+          else {
+            this.height = this.progress - 67
+          }
         }
       }
     }
   })
   export default class PasswordStrengthMeter extends Vue {}
 </script>
+
+
+<style lang="scss">
+  .progress-bar {
+    margin-left: 2px;
+    margin-right: 2px;
+    border-radius: 10px;
+  }
+</style>
