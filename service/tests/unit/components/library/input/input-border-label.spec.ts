a38db2b82897329419a1ecf7365bfd12729607d2
Andrey Doroschenko <dorosch.github.io@yandex.ru>
2020-12-26 13:23:10 +0300

    Added component factory for 'InputBorderLabel'

--- service/tests/unit/components/library/input/input-border-label.spec.ts
+++ service/tests/unit/components/library/input/input-border-label.spec.ts
@@ -3,18 +3,22 @@ import INPUT_TYPE from "@/components/library/input/consts";
 import InputBorderLabel from "@/components/library/input/InputBorderLabel.vue";
 
 describe('InputBorderLabel.vue', () => {
-  it('render label', () => {
-    const wrapper = shallowMount(InputBorderLabel, {
-      props: {id: 'test', label: 'test'}
-    })
+  function wrapperFactory(propsData = {}) {
+    return shallowMount(InputBorderLabel, {
+      props: {
+        ...propsData,
+          id: 'test',
+          label: 'test'
+      }
+    });
+  }
 
-    expect(wrapper.find('label').text()).toMatch('test')
+  it('render label', () => {
+    expect(wrapperFactory().find('label').text()).toMatch('test')
   })
 
   it('input value', () => {
-    const wrapper = shallowMount(InputBorderLabel, {
-      props: {id: 'test', label: 'test'}
-    })
+    const wrapper = wrapperFactory()
     wrapper.find('input').element.value = 'input text'
 
     expect(wrapper.find('input').element.value).toMatch('input text')
@@ -22,9 +26,7 @@ describe('InputBorderLabel.vue', () => {
 
   it('check input type', () => {
     for (const type of INPUT_TYPE) {
-      const wrapper = shallowMount(InputBorderLabel, {
-        props: {id: 'test', label: 'test', type: type}
-      })
+      const wrapper = wrapperFactory({type: type})
 
       expect(wrapper.vm.$options.props.type.validator(type)).toBe(true)
     }
